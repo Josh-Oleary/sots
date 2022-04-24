9e04c4ebe5892e64a598691f1c801887f5067d0f
Josh O <josh.oleary@gaiaherbs.com>
2022-04-23 20:16:27 -0400

    fix login-register overlap

--- sots/models/report.js
+++ sots/models/report.js
@@ -1,40 +1,41 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ReportSchema = new Schema({
-	date: {
-  		type: String,
-  		required: true
-	},
-	location: {
-  		type: String,
-  		required: true
-	},
-	video: [
-		{
-		url: String,
-		filename: String
-		}
-	],
-	author: {
-  		type: Schema.Types.ObjectId,
-  		ref: 'User'},
-	weather: {
-		type: String,
-		required: false,
-	},
-	snowpack: {
-		type: String,
-		required: false
-	},
-	avalanche: {
-		type: String,
-		required: false
-	},
-	safetyRating: {
-		type: Number,
-		required: true
-	}
+  date: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  video: [
+    {
+      url: String,
+      filename: String,
+    },
+  ],
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  weather: {
+    type: String,
+    required: false,
+  },
+  snowpack: {
+    type: String,
+    required: false,
+  },
+  avalanche: {
+    type: String,
+    required: false,
+  },
+  safetyRating: {
+    type: Number,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('Report', ReportSchema)
+module.exports = mongoose.model("Report", ReportSchema);
